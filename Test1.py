a1ed2310fbd56f9997af90d8830855e2a95536aa
mrmendee <mrmendee@gmail.com>
2023-07-25 22:07:24 +0900

    Mouse Scroll Completed.

--- Test1.py
+++ Test1.py
@@ -2,33 +2,30 @@ from tkinter import *
 
 root = Tk()
 root.title('Full Window Scrolling X Y Scrollbar Example')
-root.geometry("1000x800")
+root.geometry("1000x500")
 
 main_frame = Frame(root)
-main_frame.pack()
+canv = Canvas(main_frame,background="white")
+ybar = Scrollbar(main_frame,orient="vertical", command=canv.yview)
+scr_frame = Frame(canv,background="green")
 
-canv = Canvas(main_frame, width=500, height=300)
-frame = Frame(canv)
-ybar = Scrollbar(main_frame)
+scr_frame.bind("<Configure>",lambda e: canv.configure(scrollregion=canv.bbox("all")))
 
-def createScrollableContainer():
-    canv.config(yscrollcommand=ybar.set, highlightthickness=0)
-    ybar.config(orient=VERTICAL, command=canv.yview)
-
-    ybar.pack(fill=Y, side=RIGHT, expand=FALSE)
-    canv.pack(fill=BOTH, side=LEFT, expand=TRUE)
-    canv.create_window(0, 0, window=frame, anchor=NW)
-
-createScrollableContainer()
+canv.create_window((0, 0), window=scr_frame, anchor=NW)
+canv.configure(yscrollcommand=ybar.set, highlightthickness=0)
 
 for i in range(50):
-    label = Label(frame, text=f"Label-{i}")
-    label.grid(row=i, column=0)
+    label = Label(scr_frame, text=f"Label-{i}",background="blue",width=9)
+    label.grid(row=i, column=0,padx=1,pady=1)
+    label.bind("<MouseWheel>", lambda e: canv.yview_scroll(int(-1 * (e.delta)), "units"))
+
+main_frame.pack()
+canv.pack(fill=BOTH, side=LEFT, expand=TRUE)
+ybar.pack(fill=Y, side=RIGHT)
+
+canv.bind("<MouseWheel>", lambda e: canv.yview_scroll(int(-1 * (e.delta)), "units"))
+scr_frame.bind("<MouseWheel>", lambda e: canv.yview_scroll(int(-1 * (e.delta)), "units"))
 
 canv.update()
-canv.config(scrollregion=frame.bbox())
-
-# マウスホイールイベントをCanvasにバインドする
-canv.bind("<MouseWheel>", lambda e: canv.yview_scroll(int(-1 * (e.delta / 120)), "units"))
 
 root.mainloop()
